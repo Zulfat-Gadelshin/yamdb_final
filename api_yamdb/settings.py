e8becae67d1e63e2b3d74bb4245485c2f8782c23
Zulfat-Gadelshin <78369239+Zulfat-Gadelshin@users.noreply.github.com>
2021-10-24 10:41:48 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -23,10 +23,11 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*',
-                '130.193.52.204',
-                '127.0.0.7',
-                ]
+ALLOWED_HOSTS = [
+  '*',
+  '130.193.52.204',
+  '127.0.0.7',
+]
 
 
 # Application definition
